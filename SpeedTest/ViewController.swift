1acb663956c30651c4aaa1251cf31c6bcc54207f
Victor Quinn <mail@victorquinn.com>
2015-10-14 15:03:50 -0400

    Update

--- SpeedTest/ViewController.swift
+++ SpeedTest/ViewController.swift
@@ -23,7 +23,16 @@ class ViewController: UIViewController, LocationKitDelegate {
     }
 
     func locationKit(locationKit: LocationKit!, didUpdateLocation location: CLLocation!) {
+
         if let loc = location {
+            if (location.speed > 1) {
+                self.speed.text = "\(location.speed * 2.236936) MPH"
+            } else if (location.speed >= 0 && location.speed < 1) {
+                self.speed.text = "Stopped"
+            } else {
+                self.speed.text = "Unknown"
+            }
+            
             if loc.speed > 0 {
                 speed.text = "\(round(loc.speed * 2.236936)) MPH"
             } else {
